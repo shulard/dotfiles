c85816ed552304e727ecc9c3ff1f6ed1cfad40bb
Stéphane HULARD <s.hulard@chstudio.fr>
2019-02-03 12:58:28 +0100

    MacOS: Add missing cask

--- macos-init.sh
+++ macos-init.sh
@@ -6,7 +6,7 @@ brew install zsh git php ansible composer jq lastpass-cli weechat
 
 # Install brew Caskroom applications
 brew cask
-brew cask install keybase vlc java sequel-pro visual-studio-code blockblock oversight imageoptim kitematic transmission vagrant docker gpg-suite transmit
+brew cask install keybase vlc java sequel-pro visual-studio-code blockblock oversight imageoptim kitematic transmission vagrant docker gpg-suite transmit resilio-sync steam keka
 
 # Install specific fonts
 brew tap homebrew/cask-fonts
