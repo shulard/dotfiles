15526ba871060c0ff193c132b85dbf0475c7ee90
Stéphane HULARD <s.hulard@chstudio.fr>
2019-04-02 10:46:15 +0200

    Add MacOS defaults

--- macos-init.sh
+++ macos-init.sh
@@ -44,11 +44,40 @@ brew cask install keka
 brew tap homebrew/cask-fonts
 brew cask install font-source-code-pro
 
+# Mac OS specific configuration
 
-## Mac OS specific configuration
-
-# Enable "Tap to click" with the Magic Mouse / Magic Trackpad
+## Enable "Tap to click" with the Magic Mouse / Magic Trackpad
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 sudo defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 sudo defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 sudo defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+## Add generic keyboard shortcuts
+defaults write-g NSUserKeyEquivalents '{ Zoom = "@~="; }'
+
+## Move over all the controls using Keyboard Tab
+defaults write -g AppleKeyboardUIMode 2
+
+## Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+## Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+## Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+## Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+## Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+## Set screenshot location to ~/Pictures/Screenshots
+mkdir ~/Pictures/Screenshots
+defaults write com.apple.screencapture location ~/Pictures/Screenshots
